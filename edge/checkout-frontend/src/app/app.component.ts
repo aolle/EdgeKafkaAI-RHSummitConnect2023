f396fff219c64a1aaa21ef2e505f7e1101b9b489
Àngel Ollé Blázquez <angel@olleb.com>
2023-09-23 01:03:54 +0200

    Add main checkout component

--- edge/checkout-frontend/src/app/app.component.ts
+++ edge/checkout-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'checkout-frontend';
+  showContent = true;
+
+  constructor(private router: Router) { }
+
+  toggleContent() {
+    this.showContent = false;
+    setTimeout(() => {
+      this.router.navigate(['/checkout']);
+    }, 500);
+  }
 }
